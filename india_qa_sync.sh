7b8b4a25edcb866016a66b8665337f8803b32cf6
Kamal Dixit <kamalitexpert@gmail.com>
2020-02-10 22:55:28 +0530

    Update india_qa_sync.sh

--- india_qa_sync.sh
+++ india_qa_sync.sh
@@ -1,5 +1,15 @@
+echo "creating temp mount point"
+sudo mkdir /data_in_qa_efs
+sudo mount -t efs fs-368a2ce7:/ /data_in_qa_efs
+
+echo "copying the files from prod to qa"
+
 sudo cp -rvf /data_in_prod/* /data_in_qa
 sudo chmod -R 777 /data_in_qa/*
-echo "pre-production file count" `ls -l /data_in_qa/|wc -l`
+
+echo "qa india file count" `ls -l /data_in_qa/|wc -l`
 echo "production file count" `ls -l /data_in_prod/|wc -l`
+echo "Unmounting the temp mount point"
+sudo umount /data_in_qa_efs
+
 
