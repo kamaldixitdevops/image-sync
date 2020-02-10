f859cfde4f5aa1244b92cf7bf41dd4ccf8129367
Kamal Dixit <kamalitexpert@gmail.com>
2020-02-10 23:05:57 +0530

    Update us_qa_sync.sh

--- us_qa_sync.sh
+++ us_qa_sync.sh
@@ -3,11 +3,11 @@ sudo mkdir /data_us_qa_efs
 sudo mount -t efs fs-59903688:/ /data_us_qa_efs
 
 echo "copying us prod files to us qa"
-sudo cp -rvf /data_us_prod/* /data_us_qa
+sudo cp -rvf /data_us_prod/* /data_us_qa_efs
 sudo chmod -R 777 /data_us_qa/*
 
 echo "counting us prod and qa files"
-echo "us-qa file count" `ls -l /data_us_qa/|wc -l`
+echo "us-qa file count" `ls -l /data_us_qa_efs/|wc -l`
 echo "production file count" `ls -l /data_in_prod/|wc -l`
 
 echo "Unmounting temp mount point"
